a45f3fe0cb1bca0aa537418f8c429dc4fd6b8e02
HadisehIzadi <110168673+HadisehIzadi@users.noreply.github.com>
2024-08-03 20:03:38 +0430

    implemention

---
+++ itemScoll/app/src/main/java/com/example/itemscoll/AnimalAdaptor.java
@@ -0,0 +1,80 @@
+package com.example.itemscoll;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class AnimalAdaptor extends RecyclerView.Adapter<AnimalAdaptor.AnimalRowHolder> {
+    ArrayList<Animal> animalData;
+    Context context;
+    MyClickInterface myClickInterface;
+
+    public AnimalAdaptor(ArrayList<Animal> animals, MainActivity mainActivity, MainActivity mainActivity1) {
+    }
+
+    public void AnimalAdaptor(ArrayList<Animal>animalData , @NonNull Context context , MyClickInterface myClickInterface) {
+
+        this.context = context;
+        this.animalData = animalData;
+        this.myClickInterface = myClickInterface;
+    }
+
+    @NonNull
+    public AnimalRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.animal_row,parent,
+                                             false);
+
+        return new AnimalRowHolder(view);
+    }
+
+    public void onBindViewHolder(@NonNull AnimalRowHolder holder, int position) {
+        holder.txtAnimalName.setText(animalData.get(position).getName());
+        holder.imgAnimal.setImageResource(animalData.get(position).getImage());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return animalData.size();
+    }
+
+
+    class AnimalRowHolder extends RecyclerView.ViewHolder{
+
+        TextView txtAnimalName;
+        ImageView imgAnimal;
+
+        public AnimalRowHolder(@NonNull View itemView) {
+            super((itemView));
+
+            txtAnimalName = itemView.findViewById(R.id.txt_animal_name);
+            imgAnimal = itemView.findViewById(R.id.img_animal);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    myClickInterface.onItemClick(getAdapterPosition());
+                }
+            });
+        }
+    }
+
+
+    interface MyClickInterface{
+        void onItemClick(int postionOfTheAnimal);
+    }
+
+
+
+
+
+}
--- itemScoll/app/src/main/java/com/example/itemscoll/MainActivity.java
+++ itemScoll/app/src/main/java/com/example/itemscoll/MainActivity.java
@@ -7,12 +7,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    ArrayList<Animal> animals;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        recyclerView = findViewById(R.id.recycler_view);
+
+        animals = new ArrayList<>();
+        animals.add(new Animal("Dolphin",R.drawable.dolphin));
+        animals.add(new Animal("Lion",R.drawable.lion));
+        animals.add(new Animal("Owl",R.drawable.owl));
+
+
+
+        AnimalAdaptor animalAdapter = new AnimalAdaptor(animals,this,this);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        recyclerView.setAdapter(animalAdapter);
+
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
