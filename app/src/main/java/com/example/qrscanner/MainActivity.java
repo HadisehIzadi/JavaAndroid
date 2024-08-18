8cb319aa6c50467c3a2ac95e482f45c26d2f6886
HadisehIzadi <110168673+HadisehIzadi@users.noreply.github.com>
2024-08-18 12:28:10 +0430

    final updates

--- app/src/main/java/com/example/qrscanner/MainActivity.java
+++ app/src/main/java/com/example/qrscanner/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.qrscanner;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.Manifest;
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -12,7 +17,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     Button scanbutton;
-
+    public static final int CAMERA_REQUEST_PERMISSION = 1;
+    public static final int SCANNING_REQUEST_CODE = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +33,39 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        });
+
     }
+
+    void StartScanningAvtivity()
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                //ask for the permision
+                requestPermissions(new String[] {Manifest.permission.CAMERA},CAMERA_REQUEST_PERMISSION);
+            }else {
+                //start scanning
+                startActivityForResult(new Intent(MainActivity.this,ScanningActivity.class),SCANNING_REQUEST_CODE);
+            }
+        }else {
+            //start scanning
+            startActivityForResult(new Intent(MainActivity.this,ScanningActivity.class),SCANNING_REQUEST_CODE);
+        }
+    }
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_REQUEST_PERMISSION){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                startActivityForResult(new Intent(MainActivity.this,ScanningActivity.class),SCANNING_REQUEST_CODE);
+            }
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            if(requestCode == SCANNING_REQUEST_CODE){
+                if (resultCode == RESULT_OK){
+                    scanbutton.setText(data.getStringExtra("scanning_result"));
+                }
+            }
+        }
 }
--- app/src/main/java/com/example/qrscanner/ScanningActivity.java
+++ app/src/main/java/com/example/qrscanner/ScanningActivity.java
@@ -1,24 +1,40 @@
 package com.example.qrscanner;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
 
 public class ScanningActivity extends AppCompatActivity {
+
+    private CodeScanner mCodeScanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_scanning);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+
+        mCodeScanner = new CodeScanner(ScanningActivity.this,scannerView);
+
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull Result result) {
+                Intent intent = new Intent();
+                intent.putExtra("scanning_result",result.getText());
+                setResult(RESULT_OK,intent);
+                finish();
+            }
         });
+
+        mCodeScanner.startPreview();
+
     }
 }
