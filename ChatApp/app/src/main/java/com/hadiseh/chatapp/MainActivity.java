7d915c3479c52a9ea7c3192533e8a99cd6f3d050
HadisehIzadi <110168673+HadisehIzadi@users.noreply.github.com>
2024-08-28 17:45:05 +0430

    Firebase

--- ChatApp/app/src/main/java/com/hadiseh/chatapp/MainActivity.java
+++ ChatApp/app/src/main/java/com/hadiseh/chatapp/MainActivity.java
@@ -1,16 +1,24 @@
 package com.hadiseh.chatapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     private EditText edtUsername, edtPassword, edtEmail;
@@ -49,5 +57,63 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (edtEmail.getText().toString().isEmpty() || edtPassword.getText().toString().isEmpty()){
+                    if (isSigningUp && edtUsername.getText().toString().isEmpty()){
+                        Toast.makeText(MainActivity.this, "Invalid input", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                }
+
+                if (isSigningUp){
+                    handleSignUp();
+                }else {
+                    handleLogin();
+                }
+            }
+
+        });
+
+
         };
+
+    private void handleSignUp(){
+
+        // create user in firebase Auth
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(edtEmail.getText().toString(),edtPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                   // FirebaseDatabase.getInstance().getReference("user/"+ FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(new User(edtUsername.getText().toString(),edtEmail.getText().toString(),""));
+                  //  startActivity(new Intent(MainActivity.this,FriendsActivity.class));
+                    Toast.makeText(MainActivity.this, "Signed up successfully", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+
+        });
     }
+
+    private void handleLogin(){
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(edtEmail.getText().toString(),edtPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                 //   startActivity(new Intent(MainActivity.this,FriendsActivity.class));
+                    Toast.makeText(MainActivity.this, "Logged in successfully", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+
+
+
+}
